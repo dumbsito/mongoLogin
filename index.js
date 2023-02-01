261aad487eb98cfbde601adcaf4822fbe78f19b2
dumbsito <lean.04river@gmail.com>
2023-02-01 10:03:17 -0300

    cors

--- index.js
+++ index.js
@@ -12,7 +12,14 @@ const { equal } = require('assert')
 const path=require("path");
 
 
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
 
+app.use(cors(corsOptions)) 
 app.use(bodyParser.urlencoded({
     extended:true
 }))
